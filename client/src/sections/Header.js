f310e03e12d96d09066644bbd907d5f0d7d0b330
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-06-17 21:43:53 +0530

    created files

--- client/src/sections/Header.js
+++
@@ -1,9 +0,0 @@
-
-export default function Header() {
-
-    return (
-        <div id="Header Sec">
-            <header >& copy; 2024 all rights reserved to Jobs4u </header >
-        </div >
-    );
-}
