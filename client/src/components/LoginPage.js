3e2421a189d4e8b13dad956063b410dfb6f2fc6d
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-06-18 16:00:15 +0530

    added basic auth functionality

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,15 +1,38 @@
 // LoginPage.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth } from './AuthContext';
 import './css/loginPage.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const LoginPage = () => {
     const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+
+        e.preventDefault();
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    const USER = "asd";
+    const PASS = "asd";
 
     const handleLogin = () => {
-        login();
+
+        if (formData.username === USER && formData.password === PASS) {
+            login();
+            navigate('/profile');
+        } else {
+            alert("Hacker");
+        }
+
     };
 
     return (
@@ -20,16 +43,16 @@ const LoginPage = () => {
                 </h1>
                 <div className="d-flex flex-row">
                     <div className="login-box">
-                        <form>
+                        <form onSubmit={handleLogin}>
                             <div className="form-group">
                                 <label for="username">Username</label>
-                                <input type="text" className="form-control" id="username" placeholder="Enter username" />
+                                <input type="text" className="form-control" id="username" placeholder="Enter username" name="username" value={formData.username} onChange={handleChange} />
                             </div>
                             <div className="form-group">
                                 <label for="password">Password</label>
-                                <input type="password" className="form-control" id="password" placeholder="Enter password" />
+                                <input type="password" className="form-control" id="password" placeholder="Enter password" name="password" value={formData.password} onChange={handleChange} />
                             </div>
-                            <button className="btn btn-primary btn-block" id="login-btn" onClick={handleLogin}>Login</button>
+                            <button className="btn btn-primary btn-block" id="login-btn" type="submit">Login</button>
                         </form>
                         <p className="text-center mt-3" id="register-link">Don't have an account? <Link to='/register'>Register</Link></p>
                     </div>
